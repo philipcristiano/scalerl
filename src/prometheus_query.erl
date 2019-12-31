c7060b83c73db1766adfa3a530e09ed126273b12
Philip Cristiano <git@philipcristiano.com>
2019-12-31 14:27:55 -0500

    prometheus: Query by range
    Use a range query and then filter the data to get the HPA sizes needed

--- src/prometheus_query.erl
+++ src/prometheus_query.erl
@@ -4,54 +4,69 @@
 -export([hpa_size/2]).
 
 hpa_size(Namespace, Name) ->
-    MaxQuery = lists:flatten(["max(kube_hpa_status_current_replicas{hpa=\"",
+    DaysOfData = 1,
+    Now = os:system_time(seconds),
+    Past = Now - (DaysOfData * 86400),
+
+    Query = lists:flatten(["kube_hpa_status_current_replicas{hpa=\"",
                               binary:bin_to_list(Name),
                               "\", namespace=\"",
                               binary:bin_to_list(Namespace),
-                              "\"})"]),
-    MinQuery = lists:flatten(["min(kube_hpa_status_current_replicas{hpa=\"",
-                              binary:bin_to_list(Name),
-                              "\", namespace=\"",
-                              binary:bin_to_list(Namespace),
-                              "\"})"]),
-    MaxResp = query_instant("http://prometheus.stratobuilder.com",
-      [{query, MaxQuery}]),
+                              "\"}"]),
+    Resp = query_range("http://prometheus.stratobuilder.com",
+      [{query, Query},
+       {step, "5m"},
+       {start, Past},
+       {'end', Now}]),
 
     ?LOG_INFO(#{what => "Prometheus Response",
-                max_query => MaxResp}),
+                query => Resp}),
 
-    MaxData = maps:get(<<"data">>, MaxResp),
-    MaxResult = maps:get(<<"result">>, MaxData),
-    [MaxItem] = MaxResult,
-    MaxElement = maps:get(<<"value">>, MaxItem),
-    [_MaxTime, MaxValue] = MaxElement,
-
-
-    MinResp = query_instant("http://prometheus.stratobuilder.com",
-      [{query, MinQuery}]),
-
-    MinData = maps:get(<<"data">>, MinResp),
-    MinResult = maps:get(<<"result">>, MinData),
-    [MinItem] = MinResult,
-    MinElement = maps:get(<<"value">>, MinItem),
-    [_MinTime, MinValue] = MinElement,
-
+    Data = maps:get(<<"data">>, Resp),
+    Result = maps:get(<<"result">>, Data),
+    [Item] = Result,
+    Elements = maps:get(<<"values">>, Item),
+    % TODO: process elements
+    Values = lists:map(fun int_values_from_data/1, Elements),
+    Max = lists:max(Values),
+    Min = lists:min(Values),
 
     ?LOG_INFO(#{what => "Prometheus Metrics",
-                max_query => MaxQuery,
-                max_data => MaxData,
-                min_query => MinQuery,
-                min_data => MinData,
+                query => Query,
                 namespace => Namespace,
                 name => Name}),
-    {ok, {MinValue, MaxValue}}.
+    {ok, {Min, Max}}.
+
+int_values_from_data([_Timestamp, Value]) ->
+      {Int, _Rest} = string:to_integer(erlang:binary_to_list(Value)),
+      Int.
 
 
-query_instant(Host, Ops) ->
-    Path = Host ++ "/api/v1/query",
+% query_instant(Host, Ops) ->
+%     Path = Host ++ "/api/v1/query",
+%     Headers = [],
+%     ReqBody = {form, Ops},
+%     ?LOG_DEBUG(#{msg => "Prometheus request",
+%                  path => Path,
+%                  body => ReqBody}),
+%     {ok, Code, _Headers, ReqRef} = hackney:request(
+%       post,
+%       Path,
+%       Headers,
+%       ReqBody
+%     ),
+%     {ok, RespBody} = hackney:body(ReqRef),
+%     ?LOG_DEBUG(#{msg => "Prometheus response",
+%                 code => Code,
+%                 body => RespBody}),
+%     Data = jsx:decode(RespBody, [return_maps]),
+%     Data.
+
+query_range(Host, Ops) ->
+    Path = Host ++ "/api/v1/query_range",
     Headers = [],
     ReqBody = {form, Ops},
-    ?LOG_DEBUG(#{msg => "Prometheus request",
+    ?LOG_DEBUG(#{msg => "Prometheus range request",
                  path => Path,
                  body => ReqBody}),
     {ok, Code, _Headers, ReqRef} = hackney:request(
